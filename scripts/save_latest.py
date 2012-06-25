712f45c3eb47e84df6cbdb847396d334edae036f
Christian Fobel <christian@fobel.net>
2012-06-24 23:04:49 -0400

    Create PluginRepository class to expose json API

--- scripts/save_latest.py
+++ scripts/save_latest.py
@@ -1,12 +1,10 @@
 import sys
-import urllib
-
 from path import path
-from jsonrpc import ServiceProxy
 
 project_root = path(__file__).parent.parent
 sys.path.append(project_root)
 
+from plugin_repository import PluginRepository
 
 
 def parse_args():
@@ -16,6 +14,9 @@ def parse_args():
     parser = ArgumentParser(description="""\
 Download latest version of specified plugin.""",
                            )
+    parser.add_argument('-s', '--server_url', dest='server_url',
+            default='http://localhost:8000',
+            help='Server URL (default=%(default)s')
     parser.add_argument(nargs=1, dest='plugin_name', type=str)
     parser.add_argument(nargs=1, dest='output_dir', type=path)
 
@@ -26,27 +27,9 @@ Download latest version of specified plugin.""",
     return args
 
 
-def download_latest(plugin_name, output_dir):
-    output_dir = path(output_dir)
-    s = ServiceProxy('http://localhost:8000/json/')
-    available_plugins = s.repository.available_plugins()
-    if plugin_name not in available_plugins:
-        raise ValueError, '''\
-Plugin %s is not available.
-Available plugins include: %s''' % (plugin_name, ', '.join(
-                available_plugins))
-    latest_version = s.repository.plugin_latest_version(plugin_name)
-    plugin_url = s.repository.plugin_url(plugin_name, latest_version)
-
-    data = urllib.urlopen('http://localhost:8000%s' % plugin_url).read()
-    local_path = output_dir.joinpath(path(plugin_url).name)
-    if not local_path.isfile():
-        local_path.write_bytes(data)
-        print 'Saved latest %s to %s' % (plugin_name, local_path)
-    else:
-        print 'File %s already exists - skipping download' % (local_path)
-
-
 if __name__ == '__main__':
     args = parse_args()
-    download_latest(args.plugin_name, args.output_dir)
+    p = PluginRepository(args.server_url)
+    p.download_latest(args.plugin_name, args.output_dir)
+    print p.latest_version(args.plugin_name)
+    print p.versions(args.plugin_name)
